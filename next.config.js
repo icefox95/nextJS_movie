90e8dc32319268fbdb8dd8e21bace63a53f87058
Elfun <sparkstorms2@gmail.com>
2022-07-09 21:48:02 +0900

    Basic practice for NextJS

--- next.config.js
+++ next.config.js
@@ -1,7 +1,30 @@
+const API_KEY = process.env.API_KEY;
+
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
+  async redirects(){
+    return [
+      {
+        source:"/contact",
+        destination:"/about",
+        permanent: false,
+      }
+    ]
+  },
+  async rewrites(){
+    return [
+      {
+        source: "/api/movies",
+        destination:`https://api.themoviedb.org/3/movie/popular?api_key=${API_KEY}`,
+      },
+      {
+        source: "/api/movies/:id",
+        destination:`https://api.themoviedb.org/3/movie/:id?api_key=${API_KEY}`
+      }
+  ]
+  },
 }
 
 module.exports = nextConfig
